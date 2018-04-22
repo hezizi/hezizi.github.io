93c55bfa56e4b9c546a11aa0ac67d90647315da4
hezizi <yucihent@sina.com>
2018-04-22 09:54:04 +0800

    Site updated: 2018-04-22 09:54:03

--- js/src/topbar.js
+++ js/src/topbar.js
@@ -1,9 +1,5 @@
-var header = document.getElementById('#header');
-var body = document.querySelector('body');
-var scrollTop = body.scrollTop();
-
 window.onload = function() {
-    if(scrollTop > 100) {
-        header.style.marginTop = 91 + 'px';
+    if($(html,body).scrollTop() > 100) {
+        $('#header').css('marginTop','91px');
     }
 }
